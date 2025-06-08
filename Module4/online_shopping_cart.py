ad170b3da854eaa3e433d9ce7905b78fc4b02666
william-vann <william.vann@csuglobal.edu>
2025-06-08 10:50:51 -0500

    edits

--- Module4/online_shopping_cart.py
+++ Module4/online_shopping_cart.py
@@ -1,8 +1,9 @@
+# CSC500 William Vann
+# Module 4 Portfolio Milestone
 # Online Shopping Cart
 
 # Step 1: Create ItemToPurchase class
 '''
-
 Attributes
     item_name (string)
     item_price (float)
@@ -16,9 +17,17 @@ Method
 
 Example of print_item_cost() output:
 Bottled Water 10 @ $1 = $10
-
 '''
 
+class ItemToPurchase:
+    def __init__(self, item_name='none', item_price=0.0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+
+    def print_item_cost(self):
+        transaction_cost = self.item_price * self.item_quantity
+        print(f'{self.item_name} {self.item_quantity} @ ${self.item_price} = ${transaction_cost}')
 
 # Step 2: Prompt user for 2 items and create 2 class objects
 '''
@@ -43,7 +52,18 @@ Enter the item quantity:
 
 '''
 
+# Set number of user prompts (2 for this example)
+num_user_prompts = 2
 
+for i in range(1, num_user_prompts+1):
+    print(f'Item {i}')
+    item_name = str(input('Enter the item name:\n')))
+    item_price = float(input('Enter the item price:\n'))
+    item_quantity = int(input('Enter the item quantity:\n'))
+    
+    # Create an instance of ItemToPurchase
+    item = ItemToPurchase(item_name, item_price, item_quantity)
+    
 # Step 3: Add costs of the 2 items and print the total cost
 ''' 
 
@@ -58,3 +78,12 @@ Bottled Water 10 @ $1 = $10
 Total: $13 
 
 '''
+print(f'TOTAL COST\n')
+
+total_cost = 0.0
+
+for i in range(1, num_user_prompts+1):
+    print(f'{item}')
+print(f'\nTotal: ${total_cost}')
+
+
