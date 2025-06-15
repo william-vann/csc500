20d07cd78d6c7cd40ad6188f6ffe22a827326e5f
william-vann <william.vann@csuglobal.edu>
2025-06-15 09:51:02 -0500

    working code

--- Module5/module5_part1.py
+++ Module5/module5_part1.py
@@ -1,15 +1,33 @@
+# William Vann
+# module5_part1.py
+
+# ask user for number of years for data collection
+
 num_years = int(input('Enter the number of years: \n'))
+if num_years <= 0:
+    print('Number of years must be > 0, defaulting to 1')
+    num_years = 1 
+
+# initialize variables
 num_months = 0
 total_rainfall = 0
 
-for year in range(num_years):
+# outer loop for years
+for year in range(1,num_years+1):
+    
+    # inner loop for months
     for month in range(1,13):
-        rain_inches = int(input(f'Enter number of inches of rainfall for year {year} month {month}: \n'))
+        
+        # ask user for number of inches of rain for month in question
+        rain_inches = int(input(f'Enter inches of rainfall for year {year} month {month}: \n'))
+        # increment rainfall totals and number of months
         total_rainfall += rain_inches
         num_months += 1
 
-avg_rainfall_per_month = total_rainfall / num_months
+# calculate average rainfall per month
+avg_rainfall_per_month = round((total_rainfall / num_months), 2)
 
-print(f'Total months: {num_months}') 
-print(f'Total rainfall (in.): {total_rainfall}')
-print(f'Average rainfall (in.) per month: {avg_rainfall_per_month}')
+# output results to user 
+print(f'Total months: \t\t\t\t{num_months}') 
+print(f'Total rainfall (in.): \t\t\t{total_rainfall}')
+print(f'Average rainfall per month (in.): \t{avg_rainfall_per_month}')
--- Module5/module5_part2.py
+++ Module5/module5_part2.py
@@ -1,11 +1,26 @@
-points_dict = {
-    0: 0,
-    2: 5,
-    4: 15,
-    6: 30,
-    8: 60
+# William Vann
+# module5_part2.py
+
+# initialize books to points dictionary
+books_to_points = {
+    0:0,
+    1:0,
+    2:5,
+    3:5,
+    4:15,
+    5:15,
+    6:30,
+    7:30,
+    8:60
 }
+
+# ask user for number of books bought
 num_books_bought = int(input('Enter the number of books bought this month: \n'))
 
-if num_books_bought in points_dict:
-    print(f"You've earned {points_dict[num_books_bought]} points!")
+# conditional logic to tell user how many points they've earned
+if num_books_bought in books_to_points:
+    print(f"You've earned {books_to_points[num_books_bought]} points!")
+elif num_books_bought > 8:
+    print(f"You've earned {books_to_points[8]} points!")
+else:
+    print("Sorry, you haven't earned any points yet!")
